7bbf9487d5fd4050b596946c4f603d865d3f063b
Darshan Lila <23020201093@darshan.ac.in>
2026-09-22 11:14:06 +0530

    Refactor linked list implementation and main function
    Refactored linked list functions and updated main logic.

--- 57.c
+++ 57.c
@@ -1,4 +1,3 @@
-//  WAP to check whether 2 singly linked lists are same or not.
 #include <stdio.h>
 #include <stdlib.h>
 
@@ -8,65 +7,64 @@ typedef struct node
     struct node *link;
 } node;
 
-node *first = NULL;
-
-void insert_at_end(node *list, int x)
+node *insert_at_end(node *list, int x)
 {
-    node *New, *save;
-    New = (node *)malloc(sizeof(node));
+    node *New = (node *)malloc(sizeof(node));
     New->info = x;
     New->link = NULL;
+
     if (list == NULL)
-    {
-        list = New;
-        return;
-    }
-    save = list;
+        return New;
+
+    node *save = list;
     while (save->link != NULL)
         save = save->link;
     save->link = New;
+    return list;
 }
 
-int are_same(node *list1, node *list2)
+/* Recursive comparison */
+int are_same(node *a, node *b)
 {
-    while (list1 != NULL && list2 != NULL)
+    while (a != NULL && b != NULL)
     {
-        if (list1->info != list2->info)
-            return 0; // Lists are not same
-        list1 = list1->link;
-        list2 = list2->link;
+        if (a->info != b->info)
+            return 0;
+        a = a->link;
+        b = b->link;
     }
-    return (list1 == NULL && list2 == NULL); 
+    return (a == NULL && b == NULL);
 }
+
 void display(node *list)
 {
-    node *temp = list;
-    while (temp != NULL)
+    while (list)
     {
-        printf("%d  ", temp->info);
-        temp = temp->link;
+        printf("%d ", list->info);
+        list = list->link;
     }
     printf("\n");
 }
 
-void main()
+int main(void)
 {
-    node *list1 = (node *)malloc(sizeof(node));
-    node *list2 = (node *)malloc(sizeof(node));
-    insert_at_end(list1, 1);
-    insert_at_end(list1, 2); 
-    insert_at_end(list1, 3);
+    node *list1 = NULL, *list2 = NULL;
+
+    list1 = insert_at_end(list1, 1);
+    list1 = insert_at_end(list1, 2);
+    list1 = insert_at_end(list1, 3);
+
+    list2 = insert_at_end(list2, 1);
+    list2 = insert_at_end(list2, 2);
+    list2 = insert_at_end(list2, 3);
 
     display(list1);
-    
-    insert_at_end(list2, 1);
-    insert_at_end(list2, 2);
-    insert_at_end(list2, 3);
     display(list2);
+
     if (are_same(list1, list2))
         printf("same.\n");
     else
-        printf(" not  same.\n");
+        printf("not same.\n");
 
-
+    return 0;
 }
